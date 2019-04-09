67da792d34af566405d3adb4fad425000532cb26
Ondřej Perný <pernyond@fit.cvut.cz>
2019-04-09 20:30:38 +0200

    assignment complete

---
+++ 1_assignment/cmake-build-debug/CMakeFiles/1_assignment.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/ondra/Desktop/OSY/1_assignment/sample_tester.cpp" "C:/Users/ondra/Desktop/OSY/1_assignment/cmake-build-debug/CMakeFiles/1_assignment.dir/sample_tester.cpp.obj"
+  "C:/Users/ondra/Desktop/OSY/1_assignment/solution.cpp" "C:/Users/ondra/Desktop/OSY/1_assignment/cmake-build-debug/CMakeFiles/1_assignment.dir/solution.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
